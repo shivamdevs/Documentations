5bb068d070cf8d9943f7e5694c0f447e1cf1e4b1
Shivam Dewangan <110464857+shivamdevs@users.noreply.github.com>
2023-03-27 03:21:38 +0530

    Base deploy trial 2

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Clone the documentation directory of the OasisMenu repository
-git clone --depth=1 --branch=main https://github.com/shivamdevs/OasisMenu.git ./oasismenu
+git clone --depth=1 --branch=main https://github.com/shivamdevs/OasisMenu.git ./myoasis-docs-oasismenu
 
 # Deploy the documentation directory to the oasismenu subdirectory of your Netlify project
-netlify deploy --prod --dir=./oasismenu --prod-slug=oasismenu
+netlify deploy --prod --dir=./myoasis-docs-oasismenu/documentation --prod-slug=oasismenu
